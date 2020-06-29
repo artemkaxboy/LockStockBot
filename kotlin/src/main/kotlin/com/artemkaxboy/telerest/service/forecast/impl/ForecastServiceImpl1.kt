90c37f1e7686c36193ced1d4a5127e4691a4ccf6
Artem Kolin <artyom.kolin@eltex.loc>
2020-06-29 20:11:12 +0700

    sync

--- kotlin/src/main/kotlin/com/artemkaxboy/telerest/service/forecast/impl/ForecastServiceImpl1.kt
+++ kotlin/src/main/kotlin/com/artemkaxboy/telerest/service/forecast/impl/ForecastServiceImpl1.kt
@@ -2,15 +2,14 @@ package com.artemkaxboy.telerest.service.forecast.impl
 
 import com.artemkaxboy.telerest.config.properties.ForecastSource1Properties
 import com.artemkaxboy.telerest.converter.toLocalDateTime
+import com.artemkaxboy.telerest.dto.Forecast
 import com.artemkaxboy.telerest.dto.Source1TickerDto
 import com.artemkaxboy.telerest.service.forecast.ForecastService
 import mu.KotlinLogging
-import org.springframework.context.annotation.Configuration
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
-import java.time.LocalDate
 import java.time.LocalDateTime
-import kotlin.reflect.KFunction1
+import kotlin.math.absoluteValue
 
 private const val PAGE_SIZE = 15
 private const val MAX_PAGE = 15
@@ -19,11 +18,14 @@ private const val MAX_PAGE = 15
 class ForecastServiceImpl1(private val forecastSource1Properties: ForecastSource1Properties) :
     ForecastService {
 
+    /**
+     * Returns forecasts page from source 1.
+     */
     private fun getPage(page: Int): Array<Source1TickerDto> {
 
         val url = forecastSource1Properties.baseUrl +
             "?type=share" +
-            "&limit=$PAGE_SIZE" +
+            "&limit=${forecastSource1Properties.pageSize}" +
             "&offset=${PAGE_SIZE * page}"
 
         return RestTemplate().getForObject(url, Array<Source1TickerDto>::class.java)
@@ -38,9 +40,9 @@ class ForecastServiceImpl1(private val forecastSource1Properties: ForecastSource
             val pageResult = getPage(i)
             results.addAll(pageResult)
 
-            // if (pageResult.size < PAGE_SIZE) {
-            break
-            // }
+            if (pageResult.size < PAGE_SIZE) {
+                break
+            }
         }
 
         return results
@@ -58,19 +60,63 @@ class ForecastServiceImpl1(private val forecastSource1Properties: ForecastSource
     }
 
     private fun filterByForecasts(ticker: Source1TickerDto): Boolean {
-        logger.info { "${ticker.company.title} count ${ticker.forecasts.size}" }
-        if (ticker.forecasts.size < forecastSource1Properties.minCount) {
-            return false
+        logger.info { "${ticker.company.title} count: ${ticker.forecasts.size}" }
+
+        val remainedItemsCount = ticker.forecasts
+            .also { if (!isEnoughForecasts(it)) return false }
+
+            /* filter out old forecasts */
+            .filter(this::isActualForecast)
+            .also { if (!isEnoughForecasts(it)) return false }
+            .map { it.sharePrice }
+            .sorted()
+
+            /* cut extremely low forecast */
+            .let { cutExtremeLow(it, ticker.price) }
+            .also { if (!isEnoughForecasts(it)) return false }
+
+            /* cut extremely high forecast */
+            .reversed()
+            .let { cutExtremeLow(it, ticker.price) }
+            .also { if (!isEnoughForecasts(it)) return false }
+            .size
+
+        logger.info { "${ticker.company.title} count: $remainedItemsCount" }
+        return true
+    }
+
+    /**
+     * Drop
+     */
+    private fun cutExtremeLow(list: List<Double>, base: Double): List<Double> {
+
+        return list.takeIf { it.size > 2 }
+            ?.takeIf { ((it[0] - it[1]).absoluteValue / base) > forecastSource1Properties.extremeThreshold / 100.0 }
+            ?.also { logger.debug { "${it[0]}  ${it[1]}  $base  ${((it[0] - it[1]).absoluteValue / base)}" } }
+            ?.also { logger.debug { "dropping value ${it[0]}" } }
+            ?.drop(1)
+            ?: list
+    }
+
+    fun isEnoughForecasts(forecasts: List<Any>): Boolean {
+        if (forecasts.size < forecastSource1Properties.minCount) return false
+        return true
+    }
+
+    /**
+     * Checks if the forecast is still actual by comparing its issue date with allowed
+     * forecast [ForecastSource1Properties.ttl].
+     *
+     * @return true if given forecast is still actual, false - otherwise
+     */
+    private fun isActualForecast(forecast: Forecast): Boolean {
+        if (forecastSource1Properties.ttl.isZero) {
+            return true
         }
 
-        val actualForecasts = ticker.forecasts
-            .filter {
-                it.publishDate.toLocalDateTime()
-            }
-            .toList()
-
-        ticker.forecasts.size
-        return true
+        return forecast.publishDate.toLocalDateTime().isAfter(
+            LocalDateTime.now().minus(forecastSource1Properties.ttl)
+        )
     }
 
     companion object {
