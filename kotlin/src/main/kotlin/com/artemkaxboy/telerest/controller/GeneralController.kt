641e485799c5bf57c63152b02f18784650eaee8f
Artem Kolin <artemkaxboy@gmail.com>
2020-07-03 22:54:04 +0700

    URL redirects added

--- kotlin/src/main/kotlin/com/artemkaxboy/telerest/controller/GeneralController.kt
+++ kotlin/src/main/kotlin/com/artemkaxboy/telerest/controller/GeneralController.kt
@@ -7,12 +7,9 @@ import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiResponse
 import io.swagger.annotations.ApiResponses
-import mu.KotlinLogging
 import org.springframework.http.MediaType
 import org.springframework.http.server.reactive.ServerHttpRequest
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
@@ -31,21 +28,5 @@ class GeneralController(
     @ApiResponses(value = [ApiResponse(code = 200, message = "OK")])
     private fun getVersion(@ApiIgnore request: ServerHttpRequest): Mono<ResponseDto> {
         return ResponseDto.getResponse(request) { applicationProperties.version }.toMono()
-    }
-
-    @PostMapping(
-        "/echo",
-        consumes = [MediaType.TEXT_PLAIN_VALUE],
-        produces = [MediaType.APPLICATION_JSON_VALUE]
-    )
-    @ApiOperation(value = "Get echo")
-    @ApiResponses(value = [ApiResponse(code = 200, message = "OK")])
-    private fun getEcho(@RequestBody requestBody: String, @ApiIgnore request: ServerHttpRequest): Mono<String> {
-        logger.info { request }
-        return requestBody.also { logger.info { it } }.toMono()
-    }
-
-    companion object {
-        private val logger = KotlinLogging.logger { }
     }
 }
---
+++ kotlin/src/main/kotlin/com/artemkaxboy/telerest/controller/RootRedirectController.kt
@@ -0,0 +1,21 @@
+package com.artemkaxboy.telerest.controller
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.server.reactive.ServerHttpResponse
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+import springfox.documentation.annotations.ApiIgnore
+import java.net.URI
+
+@ApiIgnore
+@RestController
+class RootRedirectController {
+
+    @GetMapping(value = ["/", "/api", "/api/"])
+    private fun getEcho(response: ServerHttpResponse): Mono<Void> {
+        response.statusCode = HttpStatus.MOVED_PERMANENTLY
+        response.headers.location = URI.create("/swagger-ui.html")
+        return response.setComplete()
+    }
+}
