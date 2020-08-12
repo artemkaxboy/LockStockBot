d8a4be77fcbc47e381feed99fb54bdb1023044bb
Artem Kolin <artemkaxboy@gmail.com>
2020-08-12 18:49:16 +0700

    squash! Add telegram /list command
    - exception utils encapsulating

--- kotlin/src/main/kotlin/com/artemkaxboy/telerest/tool/ExceptionUtils.kt
+++ kotlin/src/main/kotlin/com/artemkaxboy/telerest/tool/ExceptionUtils.kt
@@ -2,8 +2,16 @@ package com.artemkaxboy.telerest.tool
 
 object ExceptionUtils {
 
+    fun String.prefixReasonIfNeeded(prefix: String? = null): String {
+        return prefix
+            .takeUnless { it.isNullOrBlank() }
+            ?.let { this.prefixReason(it) }
+            ?: this
+    }
+
+    fun String.prefixReason(prefix: String): String = "$prefix: $this"
+
     fun Throwable.getMessage(prefix: String? = null): String {
-        val mainMessage = prefix?.let { "$it: " } ?: ""
-        return "$mainMessage${message ?: toString()}"
+        return (message ?: toString()).prefixReasonIfNeeded(prefix)
     }
 }
