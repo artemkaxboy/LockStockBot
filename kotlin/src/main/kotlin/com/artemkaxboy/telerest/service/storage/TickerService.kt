8d951faae5078cc4900540ae4a17be51c8688a64
Artem Kolin <artemkaxboy@gmail.com>
2020-08-16 19:32:46 +0700

    - return nulls from service

--- kotlin/src/main/kotlin/com/artemkaxboy/telerest/service/storage/TickerService.kt
+++ kotlin/src/main/kotlin/com/artemkaxboy/telerest/service/storage/TickerService.kt
@@ -2,7 +2,10 @@ package com.artemkaxboy.telerest.service.storage
 
 import com.artemkaxboy.telerest.entity.Ticker
 import com.artemkaxboy.telerest.repo.TickerRepo
+import com.artemkaxboy.telerest.tool.ExceptionUtils.getMessage
+import com.artemkaxboy.telerest.tool.Result
 import com.artemkaxboy.telerest.tool.sorting.Sorting
+import mu.KotlinLogging
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.CrudRepository
@@ -22,8 +25,13 @@ class TickerService(
      *
      * @see PagingAndSortingRepository.findAll
      */
-    fun findAll(pageRequest: Pageable = defaultPageRequest): Page<Ticker> =
-        tickerRepo.findAll(defaultSortIfUnsorted(pageRequest))
+    fun findAll(pageRequest: Pageable = defaultPageRequest): Page<Ticker>? {
+        val error = "Cannot find tickers"
+        return Result
+            .of(error) { tickerRepo.findAll(defaultSortIfUnsorted(pageRequest)) }
+            .onFailure { logger.warn { it.getMessage() } }
+            .getOrNull()
+    }
 
     /**
      * Finds entity by id.
@@ -31,12 +39,28 @@ class TickerService(
      * @see CrudRepository.findByIdOrNull
      * @return found entity or null
      */
-    fun findById(tickerId: String): Ticker? = tickerRepo.findByIdOrNull(tickerId)
+    fun findById(tickerId: String): Ticker? {
+        val error = "Cannot find ticker $tickerId"
+        return Result
+            .of(error) { tickerRepo.findByIdOrNull(tickerId) }
+            .onFailure { logger.warn { it.getMessage() } }
+            .getOrNull()
+    }
 
     /**
      * Saves entity to repo.
      *
      * @see CrudRepository.save
      */
-    fun save(ticker: Ticker): Ticker = tickerRepo.save(ticker)
+    fun save(ticker: Ticker): Ticker? {
+        val error = "Cannot save ticker ${ticker.id}"
+        return Result
+            .of(error) { tickerRepo.save(ticker) }
+            .onFailure { logger.warn { it.getMessage() } }
+            .getOrNull()
+    }
+
+    companion object {
+        private val logger = KotlinLogging.logger { }
+    }
 }
